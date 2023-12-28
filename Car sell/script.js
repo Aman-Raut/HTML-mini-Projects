b70fff35799f7b51a6c643f1ee62c3d1f667f7f2
Aman Raut <92941846+Aman-Raut@users.noreply.github.com>
2023-12-28 20:44:38 +0530

    Car Sell

---
+++ Car sell/script.js
@@ -0,0 +1,16 @@
+function buyCar() {
+    alert('Thank you for your purchase!');
+  }
+  
+  function login() {
+    // Logic for handling login
+  }
+  
+  function logout() {
+    // Logic for handling logout
+  }
+  
+  function submitContactForm() {
+    // Logic for handling contact form submission
+  }
+  
